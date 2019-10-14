29239c63424f58c388bc90313444ba97a9b5dcf6
alsabila.shakina <alsabila.shakina@ui.ac.id>
2019-10-14 14:00:38 +0700

    Menyelesaikan latihan Lazy Evaluation

--- latihanLazyEvaluation.hs
+++ latihanLazyEvaluation.hs
@@ -1,5 +1,7 @@
 -- Lazy Evaluation
 
+import Data.List
+
 -- NO 1
 {-
 1. Ekspresi mengambil input list x = [1,2,3,4] dan list y = [2,3,4]
@@ -15,3 +17,32 @@ quickSort [] = []
 quickSort (x:xs) = quickSort [y | y <- xs, y <= x] 
                    ++ [x] ++ 
                    quickSort [y | y <- xs, y > x]
+
+-- NO 4
+-- x = [2,3,4] \\ [3] artinya mengeluarkan 3 dari list x, jadi output x = [2,4]
+perm [] = [[]]
+perm ls = [ x:ps | x <- ls, ps <- perm(ls\\[x])]
+
+-- NO 5
+primes = sieve [2 .. 10]
+  where sieve (x:xs) = x : (sieve [z | z <- xs, z `mod` x /= 0])
+{-
+Saringan Erastosthenes
+1. Tulis semua bilangan, mulai dari 1 sampai n. Misalkan ini adalah daftar A.
+2. Buat suatu daftar yang masih kosong, sebut saja daftar B.
+3. Coret bilangan 1 dari daftar A.
+4. Lalu tulis 2 pada daftar B. Lalu coret 2 dan semua kelipatannya dari daftar A
+5. Bilangan pertama yang belum tercoret dari daftar A (misalnya 3) adalah bilangan prima. Tulis bilangan ini di daftar B, lalu coret bilangan ini dan semua kelipatannya dari daftar A.
+6. Ulangi langkah 4 sampai semua bilangan di daftar A sudah tercoret.
+-}
+-- Contoh soal no 5 mencari bilangan prima dari 2 sampai 10
+-- z `mod` x \= 0 artinya bilangan z tidak habis dibagi x, jadi bukan kelipatannya
+-- Outputnya keluar, tapi ada error Non-exhaustive patterns in function sieve, mungkin karena sieve [] belum didefinisikan.
+
+
+-- NO 6
+pythaTriple = [(x,y,z) |  z <- [5 ..]
+                        , y <- [z, z-1 .. 1]
+                        , x <- [y, y-1 .. 1]
+                        , x*x + y*y == z*z ]
+-- segitiga pythagoras pertama adalah (3,4,5) jadi z >= 5, y pasti lebih kecil dari z dan x pasti lebih kecil dari y

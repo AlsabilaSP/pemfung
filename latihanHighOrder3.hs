49c1a1053c5648a80f905964eb0509a0292864cc
alsabila.shakina <alsabila.shakina@ui.ac.id>
2019-10-14 12:49:02 +0700

    Mengerjakan no 3 bagian 3

--- latihanHighOrder3.hs
+++ latihanHighOrder3.hs
@@ -1,11 +1,17 @@
 -- 3. Generating Lists
 
-import Test.QuickCheck
-import Text.Show.Functions
+-- Saya tidak bisa import Test.QuickCheck :') jadi tidak bisa mengejakan soal 1 dan 2
 
-import Data.Char
-import Data.Maybe
-import Data.List
+-- NO 3
+prop_ZipUnzip :: [(Int,Int)] -> Bool
+prop_ZipUnzip xys =
+  zip xs ys == xys
+ where
+  (xs,ys) = unzip xys
 
-listOfLength :: Int -> Gen a -> Gen [a]
-listOfLength n gen = sequence [ gen | i <- [1..n] ]
+-- Ingat zip :: [a] -> [b] -> [(a,b)]
+-- Ingat unzip :: [(a,b)] -> ([a], [b])
+-- Fungsi prop_ZinUnzip mengambil input xys bertipe [(Int,Int)], outputnya adalah nilai kebenaran dari zip xs ys == xys dimana (xs,yx) adalah output dari unzip xys
+-- xys bertipe [(a,b)], unzip xys akan mengeluarkan ([a],[b]), xs = [a] dan ys = [b]
+-- zip xs ys atau zip [a] [b] akan mengeluarkan [(a,b)], dimana [(a,b)] == xys
+-- Jadi, seharusnya outputnya True

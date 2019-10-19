fe17773e7f1924de3b1adaae9b1fd9594e23e980
Alsabila Shakina Prasetyo <36217256+AlsabilaSP@users.noreply.github.com>
2019-10-19 15:04:02 +0700

    Membuat fungsi evaluate dengan foldr

--- latihanAritmatika.hs
+++ latihanAritmatika.hs
@@ -36,10 +36,17 @@ evaluate (V _) = 0.0
 -- Menurut saya evaluate (V _) sudah correct. evaluate menerima Expr (V _) dan _ bisa apa saja lalu mengembalikan 0.0
 
 -- Fungsi evaluasi dengan map masa percobaan
-mapEval :: (Expr -> Float) -> Expr -> Expr
+mapEval :: (Expr -> Float) -> Expr -> Float
 -- Asumsi f di sini adalah evaluate
 mapEval f (C x) = (C x)
 mapEval f (e1 :+ e2) = (mapExpr f e1) :+ (mapExpr f e2)
 mapEval f (e1 :- e2) = (mapExpr f e1) :- (mapExpr f e2)
 mapEval f (e1 :* e2) = (mapExpr f e1) :* (mapExpr f e2)
 mapEval f (e1 :/ e2) = (mapExpr f e1) :/ (mapExpr f e2)
+
+-- Fungsi evaluasi dengan foldr masa percobaan
+-- Asumsi f di sini adalah evaluate
+foldEval f (e1 :+ e2) = foldr (+) (f e1) ([f e2])
+foldEval f (e1 :- e2) = foldr (-) (f e1) ([f e2])
+foldEval f (e1 :* e2) = foldr (*) (f e1) ([f e2])
+foldEval f (e1 :/ e2) = foldr (/) (f e1) ([f e2])
